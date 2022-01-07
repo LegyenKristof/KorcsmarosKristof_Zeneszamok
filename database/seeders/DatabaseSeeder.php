85b1b4a996b32f0e9002a24bce47123f5824a20a
Your Name <you@example.com>
2022-01-07 14:41:46 +0100

    Seeder kész

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
         \App\Models\Song::factory(10)->create();
     }
 }
